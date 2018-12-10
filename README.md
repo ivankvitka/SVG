1bd00c46ae4fab32ab33202afa829a75c5828b3a
ivankvitka <ivan.kvitka@gmail.com>
2018-12-10 12:33:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Modal Window
+[link to github page](https://ivankvitka.github.io/ModalWindow/dest/index.html)
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	browserSync = require("browser-sync"),
+	plumber = require("gulp-plumber");
+
+gulp.task('browser-sync', function () {
+	browserSync({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false
+	});
+});
+
+gulp.task("sass", function () {
+	gulp
+		.src("app/scss/**/*.scss")
+		.pipe(plumber())
+		.pipe(sass({outputStyle: "expanded"}))
+		.pipe(gulp.dest("app/css"))
+		.pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', ['browser-sync', 'sass'], function () {
+	gulp.watch('app/scss/*.+(sass|scss)', ['sass']);
+	gulp.watch('app/*.html', browserSync.reload);
+});
+
+gulp.task('build', function () {
+	gulp.src('app/css/*.css')
+		.pipe(gulp.dest('build/css'));
+	gulp.src('app/*.html')
+		.pipe(gulp.dest('build'));
+});
